40071842402eabbc7805f1f727ae7af3a584e349
janman27929 <janman27929@gmail.com>
2016-06-04 04:37:42 -0400

    git test 2

---
+++ jjs3.pm
@@ -0,0 +1,73 @@
+package jjs3;
+
+use lib qw(. .. lib /tools/lib2);
+
+use strict;
+use warnings;
+use misc;
+
+#---------[ CLASS Variables  ]------------
+# %s/jjs3/jjs2/g
+# sav jjs3.pm
+
+#---------[ CLASS Variables  ]------------
+my %hDefaults = (
+	debug => 1,
+);
+
+__PACKAGE__->new(
+)->_run unless caller;
+
+sub _run { 
+	my ($self) = @_; 
+	eval {
+		$self->setup();
+		$self->main ();
+		$self->teardown();
+	};
+
+	if ($@) {
+		die ("FAIL: $@ \n");
+	}	
+}
+
+
+#---------[ CLASS Variables  ]------------
+
+#---------[ PUBLIC METHODS ]------------
+sub new { 
+	my $class = shift;	
+	my %hParms = @_;
+
+	my $self = {
+		%hDefaults,
+		%hParms,
+	};
+
+	bless ($self, $class); 
+	$self;
+}; 
+
+sub setup { 
+	my $self = shift;	
+	my $nRtn = 0;
+	return $nRtn;
+}
+
+sub main { 
+	my $self = shift;	
+	my $nRtn = 0;
+	return $nRtn;
+}
+
+sub teardown { 
+	my $self = shift;	
+	my $nRtn = 0;
+	return $nRtn;
+}
+
+#---------[ PRIVATE METHODS  ]------------
+
+1;
+
+
